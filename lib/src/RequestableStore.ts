e3ef97155f86adc0255abd85da54c2882f8cbffb
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-10-23 22:22:26 +0600

    Add withRequest.bound

--- lib/src/RequestableStore.ts
+++ lib/src/RequestableStore.ts
@@ -40,6 +40,34 @@ export function withRequest<T>(
   };
 }
 
+withRequest.bound = function bound<T>(
+  target: RequestableStore<any, any>,
+  propertyKey: string | symbol,
+  descriptor: TypedPropertyDescriptor<AsyncAction<T>>
+): TypedPropertyDescriptor<AsyncAction<T>> {
+  return {
+    configurable: true,
+    enumerable: false,
+
+    get(this: typeof target) {
+      const { value, get, set, ...rest } = descriptor;
+      const fn = value!;
+      const self = this;
+
+      Object.defineProperty(this, propertyKey, {
+        ...rest,
+        async value(...params: any[]) {
+          const t = await self.request(() => fn.call(self, ...params));
+          return t.get();
+        },
+      });
+
+      return this[propertyKey];
+    },
+    set: () => {},
+  };
+};
+
 export default class RequestableStore<RS extends object, UIS extends UIStore<RS>> extends BaseStore<
   RS
 > {
