d88f22350cf15a747b2b7d581a647aeb4d642ca5
Vladimir <vzh.inbox@gmail.com>
2019-05-08 13:21:10 +0600

    Remove typedInject from default exports

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,7 +2,6 @@ export { default as Notification } from './Notification';
 export * from './Notification';
 export { default as ValidableModel } from './ValidableModel';
 export * from './ValidableModel';
-export { default as typedInject } from './typedInject';
 export { default as ReactionDisposer } from './ReactionDisposer';
 export { default as BaseRootStore } from './BaseRootStore';
 export { default as BaseStore } from './BaseStore';
