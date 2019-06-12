ec68abdc153bd94aed66fc001e03f74c3e3fb997
Vladimir <vzh.inbox@gmail.com>
2019-06-12 14:09:17 +0600

    Improve stateStorage

--- src/storage/stateStorage.ts
+++ src/storage/stateStorage.ts
@@ -6,14 +6,20 @@ export type JSONStoreState<RootState extends {}> = Required<JSONModel<RootState>
 
 export async function saveRootState<RootState extends {}>(
   storage: LocalForageDbMethodsCore,
-  state: Partial<JSONStoreState<RootState>>
+  state: Partial<JSONStoreState<RootState>>,
+  throwError: boolean = false
 ): Promise<void> {
-  await Promise.all(
-    Object.getOwnPropertyNames(state).map(async prop => {
-      const prevState = await storage.getItem<{} | undefined>(prop);
-      await storage.setItem(prop, prevState ? { ...prevState, ...state[prop] } : state[prop]);
-    })
-  );
+  try {
+    await Promise.all(
+      Object.getOwnPropertyNames(state).map(async prop => {
+        const prevState = await storage.getItem<{} | undefined>(prop);
+        await storage.setItem(prop, prevState ? { ...prevState, ...state[prop] } : state[prop]);
+      })
+    );
+  } catch (ex) {
+    if (throwError) throw ex;
+    console.error(ex.message || ex);
+  }
 }
 
 export async function getStoreState<
@@ -22,9 +28,16 @@ export async function getStoreState<
 >(
   storage: LocalForageDbMethodsCore,
   store: K,
-  mergeWith?: Partial<JSONStoreState<RootState>[K]>
+  mergeWith?: Partial<JSONStoreState<RootState>[K]>,
+  throwError: boolean = false
 ): Promise<Option<JSONStoreState<RootState>[K]>> {
-  const state = await storage.getItem<JSONStoreState<RootState>[K] | undefined>(store as string);
-  if (!state && !mergeWith) return None;
-  return Option.of(mergeWith ? { ...mergeWith, ...state } : state);
+  try {
+    const state = await storage.getItem<JSONStoreState<RootState>[K] | undefined>(store as string);
+    if (!state && !mergeWith) return None;
+    return Option.of(mergeWith ? { ...mergeWith, ...state } : state);
+  } catch (ex) {
+    if (throwError) throw ex;
+    console.error(ex.message || ex);
+    return None;
+  }
 }
