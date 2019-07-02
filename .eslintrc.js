14d7ed250252cce8eeb78863177864a6e120f08a
Vladimir <vzh.inbox@gmail.com>
2019-07-02 14:11:55 +0600

    Update deps and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
+  root: true,  
   extends: require.resolve('@vzh/configs/eslint/ts.common.eslintrc.js'),
-  
   rules: {
     'no-empty-function': 'off',
     '@typescript-eslint/array-type': 'off',
