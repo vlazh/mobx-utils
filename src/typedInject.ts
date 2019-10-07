f8f25f94e6625e7bbb06b1135076cfcff532baed
Vladimir <vzh.inbox@gmail.com>
2019-10-07 16:06:14 +0600

    Remove typedInject

--- src/typedInject.ts
+++
@@ -1,9 +0,0 @@
-import { Diff } from '@vzh/ts-types';
-import { inject, IReactComponent, IStoresToProps } from 'mobx-react';
-
-type TypedInject = <I extends object, P extends object, S = any>(
-  mapStoreToProps: IStoresToProps<S, P, I>
-) => (component: IReactComponent<P & I>) => IReactComponent<Diff<P, I>>;
-
-const typedInject = inject as TypedInject;
-export default typedInject;
