30d18709f209715e82e53c816cc8ad66af3e05ff
Vladimir <vzh.inbox@gmail.com>
2019-10-21 12:52:56 +0600

    Add setLoading

--- src/stores/LoadableStore.ts
+++ src/stores/LoadableStore.ts
@@ -1,4 +1,5 @@
-import { observable, computed } from 'mobx';
+/* eslint-disable @typescript-eslint/unbound-method */
+import { observable, computed, action } from 'mobx';
 import BaseStore from './BaseStore';
 
 export default abstract class LoadableStore<
@@ -8,14 +9,33 @@ export default abstract class LoadableStore<
   @observable
   protected pendingRequestCount = 0; // For multiple requests
 
+  constructor(rootStore: RS) {
+    super(rootStore);
+    this.setLoading = this.setLoading.bind(this);
+  }
+
   /** true - while has at least 1 running request. */
   @computed
   get loading(): boolean {
     return this.pendingRequestCount > 0;
   }
 
+  // /**
+  //  * Increment or decrement running requests number.
+  //  * In some cases (in async code?) get error: [mobx] The setter of computed value is trying to update itself...
+  //  * https://github.com/mobxjs/mobx/blob/master/src/core/computedvalue.ts#L171
+  //  */
+  // set loading(value: boolean) {
+  //   if (this.pendingRequestCount === 0 && !value) {
+  //     return;
+  //   }
+  //   this.pendingRequestCount += value ? 1 : -1;
+  // }
+
   /** Increment or decrement running requests number. */
-  set loading(value: boolean) {
+  @action
+  setLoading(value: boolean): void {
+    console.log(this);
     if (this.pendingRequestCount === 0 && !value) {
       return;
     }
