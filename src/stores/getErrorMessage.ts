b1326859413892db067851aabbec1ab756b5f939
Vladimir <vzh.inbox@gmail.com>
2020-05-17 22:32:07 +0600

    Improve getErrorMessage

--- src/stores/getErrorMessage.ts
+++ src/stores/getErrorMessage.ts
@@ -3,5 +3,6 @@ import { Throwable } from '@vzh/ts-types/fp';
 export default function getErrorMessage(error: Throwable): string {
   if (typeof error === 'string') return error;
   if (Object.getOwnPropertyNames(error).includes('toString')) return error.toString();
+  if (error.constructor === {}.constructor) return (error as Error).message;
   return `${error.constructor.name}: ${(error as Error).message}`;
 }
