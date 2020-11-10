b5e3e74b237027636294c729f89eded5b7dc4ce5
Vladimir <vzh.inbox@gmail.com>
2020-11-10 14:33:09 +0600

    Add simple object store utils

---
+++ src/stores/object.ts
@@ -0,0 +1,126 @@
+import { transaction, toJS } from 'mobx';
+
+export type StateLike<S extends AnyObject> = ExcludeKeysOfType<S, AnyFunction>;
+
+export interface StoreMethods<S extends AnyObject> {
+  init: (initialState: StateLike<S>) => void;
+  update: (patch: Partial<StateLike<S>>) => void;
+  reset: VoidFunction;
+}
+
+type States = { [P: string]: StateLike<AnyObject> };
+
+export type JSStates<S extends States> = { [P in keyof StateLike<S>]: StateLike<S[P]> };
+
+export interface RootStoreMethods<S extends States> {
+  init: (states: Partial<JSStates<S>>) => void;
+  update: (states: Partial<JSStates<S>>) => void;
+  resetAll: VoidFunction;
+  toJS: () => JSStates<S>;
+}
+
+export type StoreLike<S extends AnyObject> = S & StoreMethods<S>;
+
+export type RootStoreLike<S extends States> = S & RootStoreMethods<S>;
+
+const storeSymbol = Symbol.for('__mobx_object_store__');
+const rootStoreSymbol = Symbol.for('__mobx_object_root_store__');
+const storeSymbolProp = '@@__mobx_object_store__';
+const rootStoreSymbolProp = '@@__mobx_object_root_store__';
+
+export function isStore<S extends AnyObject>(value: AnyObject): value is StoreLike<S> {
+  return (
+    typeof value === 'object' &&
+    value[storeSymbolProp] === storeSymbol &&
+    typeof (value as StoreLike<S>).init === 'function' &&
+    typeof (value as StoreLike<S>).update === 'function' &&
+    typeof (value as StoreLike<S>).reset === 'function'
+  );
+}
+
+export function isRootStore<S extends States>(value: AnyObject): value is RootStoreLike<S> {
+  return (
+    typeof value === 'object' &&
+    value[rootStoreSymbolProp] === rootStoreSymbol &&
+    typeof (value as RootStoreLike<S>).init === 'function' &&
+    typeof (value as RootStoreLike<S>).update === 'function' &&
+    typeof (value as RootStoreLike<S>).resetAll === 'function'
+  );
+}
+
+export function updateState<S extends AnyObject>(state: S, patch: Partial<S>): S {
+  Object.getOwnPropertyNames(patch).forEach((prop) => {
+    if (typeof patch[prop] !== 'function' && prop in state) {
+      // eslint-disable-next-line no-param-reassign
+      state[prop] = patch[prop as keyof S];
+    }
+  });
+  return state;
+}
+
+export function createStore<S extends AnyObject>(initialState: S): StoreLike<S> {
+  let initial: StateLike<S> = initialState;
+  return {
+    ...initialState,
+    [storeSymbolProp]: storeSymbol,
+
+    init(state) {
+      initial = state;
+      this.update(state);
+    },
+
+    update(patch) {
+      updateState(this, patch);
+    },
+
+    reset() {
+      this.update(initial);
+    },
+  };
+}
+
+export function createRootStore<S extends States>(stores: S): RootStoreLike<S> {
+  return {
+    ...stores,
+    [rootStoreSymbolProp]: rootStoreSymbol,
+
+    init(states) {
+      transaction(() => {
+        Object.getOwnPropertyNames(states).forEach((prop) => {
+          if (states[prop] && typeof states[prop] !== 'function' && isStore(this[prop])) {
+            (this[prop] as StoreLike<AnyObject>).init(states[prop]);
+          }
+        });
+      });
+    },
+
+    update(states) {
+      transaction(() => {
+        Object.getOwnPropertyNames(states).forEach((prop) => {
+          if (states[prop] && typeof states[prop] !== 'function' && isStore(this[prop])) {
+            (this[prop] as StoreLike<AnyObject>).update(states[prop]);
+          }
+        });
+      });
+    },
+
+    resetAll() {
+      transaction(() => {
+        Object.getOwnPropertyNames(this).forEach((prop) => {
+          if (typeof this[prop] !== 'function' && isStore(this[prop])) {
+            (this[prop] as StoreLike<AnyObject>).reset();
+          }
+        });
+      });
+    },
+
+    toJS() {
+      return Object.getOwnPropertyNames(this).reduce((acc, prop) => {
+        if (typeof this[prop] !== 'function' && isStore(this[prop])) {
+          acc[prop] = toJS(this[prop]);
+        }
+        return acc;
+      }, {} as JSStates<S>);
+    },
+  };
+}
